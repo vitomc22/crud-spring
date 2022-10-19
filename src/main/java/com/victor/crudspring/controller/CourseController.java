230fc8036d50fc5b57dd9318f37ef4835419d606
Victor Oliveira <vimatozin@hotmail.com>
2022-10-18 21:51:55 -0300

    Criado método Post -> created

--- src/main/java/com/victor/crudspring/controller/CourseController.java
+++ src/main/java/com/victor/crudspring/controller/CourseController.java
@@ -1,28 +1,34 @@
 package com.victor.crudspring.controller;
 
-import java.util.List;
-
 import com.victor.crudspring.model.Course;
 import com.victor.crudspring.repository.CourseRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
 
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
     public CourseController(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     } // usando injeção de dependencia via construtor ao invés do Autowired
-      // boa prática
+    // boa prática
 
     @GetMapping // mesmo que @RequestMapping(method.get)
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+
+    }
+
 
 }
