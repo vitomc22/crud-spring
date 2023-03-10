8842214bcb34b0902602a2af57568f5cd3df1592
victor <vimatozin@hotmail.com>
2023-03-10 16:37:34 -0300

    Created class DTO using record java type

---
+++ src/main/java/com/victor/crudspring/dto/CourseDTO.java
@@ -0,0 +1,13 @@
+package com.victor.crudspring.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+public record CourseDTO(Long id,
+                        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
+                        @NotBlank(message = "A categoria não pode ser vazia!") @Size(min = 1, max = 10) @NotNull @Pattern(regexp = "") @Column(length = 10, nullable = false) String category) {
+}
