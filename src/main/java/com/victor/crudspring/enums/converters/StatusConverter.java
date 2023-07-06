74ed24abf052886c00eb50eb3d5d7a5f8179a0f8
victor oliveira <vimatozin@hotmail.com>
2023-07-06 09:22:44 -0300

    Created Category converter DTO Record

---
+++ src/main/java/com/victor/crudspring/enums/converters/StatusConverter.java
@@ -0,0 +1,28 @@
+package com.victor.crudspring.enums.converters;
+
+import com.victor.crudspring.enums.Status;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class StatusConverter implements AttributeConverter<Status, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Status status) {
+        if (status == null) {
+            return null;
+        }
+        return status.getValue();
+    }
+
+    @Override
+    public Status convertToEntityAttribute(String value) {
+        if (value == null) {
+            return null;
+        }
+        return Stream.of(Status.values()).filter(c -> c.getValue().equals(value)).findFirst().orElseThrow(IllegalArgumentException::new);
+
+    }
+}
