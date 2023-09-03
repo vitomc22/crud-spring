6eff9616698da68d560df9cb733e1a835f4fb13b
Victor Oliveira <vimatozin@hotmail.com>
2023-09-03 18:58:02 -0300

    Added mokito tests

--- src/main/java/com/victor/crudspring/exception/RecordNotFoundException.java
+++ src/main/java/com/victor/crudspring/exception/RecordNotFoundException.java
@@ -8,6 +8,6 @@ public class RecordNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public RecordNotFoundException(Long id) {
-        super("Registro não encotrado com o id: " + id);
+        super("Registro não encontrado com o id: " + id);
     }
 }
--- src/test/java/com/victor/crudspring/CourseControllerTest.java
+++ src/test/java/com/victor/crudspring/CourseControllerTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.victor.crudspring.controller.CourseController;
 import com.victor.crudspring.dto.CourseDTO;
 import com.victor.crudspring.dto.LessonDTO;
+import com.victor.crudspring.exception.RecordNotFoundException;
 import com.victor.crudspring.service.CourseService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,7 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,20 +39,44 @@ public class CourseControllerTest {
     @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller).alwaysDo(print()).build();
+        lesson = new LessonDTO(1L, "primeira aula", "joiwjpih");
+        course = new CourseDTO(2L, "Cpp pro", "Back-end", Stream.of(lesson).toList());
     }
 
     @Test
     void listAllCourses() throws Exception {
         when(service.list()).thenReturn(Collections.singletonList(course));
 
-        mockMvc.perform(get("/course/list").contentType("aplication/json"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/courses").contentType("aplication/json")).andExpect(status().isOk()).andReturn();
         verify(service).list();
         verifyNoMoreInteractions(service);
     }
 
     @Test
-    void findByid() throws Exception {
+    void findCourseByid() throws Exception {
+        when(service.findByid(course.id())).thenReturn(course);
+
+        mockMvc.perform(get("/api/courses/{id}", course.id()).contentType("aplication/json")).andExpect(status().isOk()).andReturn();
+        verify(service).findByid(course.id());
+        verifyNoMoreInteractions(service);
+
+
+    }
+
+    @Test()
+    void ShouldBeReturnExceptionFindCourseWithoutByid() throws Exception {
+        RecordNotFoundException recordNotFoundException = Assertions.assertThrows(RecordNotFoundException.class, () -> {
+            mockMvc.perform(get("/api/courses/{id}", 1000).contentType("aplication/json"));
+        });
+        String expectedMessage = "Registro não encontrado com o id: " + course.id();
+        String actualMessage = recordNotFoundException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+        verify(service).findByid(course.id());
+        verifyNoInteractions(service);
+
+
+
+
 
     }
 
